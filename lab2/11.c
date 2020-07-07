531d18ad7d9d322b6832d3efefbf84a3734b1efa
1834902542 <67390379+1834902542@users.noreply.github.com>
2020-07-07 21:05:36 +0600

    Add files via upload

---
+++ lab2/11.c
@@ -0,0 +1,46 @@
+/* Delete a value “X” (take input X from user) from
+   the array shift all other value to left. If “X”
+   is not present in the given array print Element
+   Not Found. Delete all the occurrence of the value.
+   And print the whole array */
+
+#include<stdio.h>
+int main ()
+{
+    int arr[50] = {1, 2, 3, 4, 5, 6, 4, 7, 8, 9,10,11,12};
+    int i, j, x,count=0, size=12;
+    printf("Delete X value\n");
+    scanf("%d",&x);
+
+    for(i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    printf("\n");
+
+    for(j=0;j<=size;j++){
+
+        if(x==arr[j]){
+
+            for(i=j;i<=size;i++){
+                arr[i] = arr[i+1];
+            }
+            size--;
+            count++;
+            j--;
+        }
+    }
+
+    if(count==0)
+    {
+        printf(" Element Not Found\n");
+    }
+    else
+    {
+        for(i=0;i<size;i++){
+            printf("%d ",arr[i]);
+        }
+    }
+
+    return 0;
+}
